5d231f997579aa057b02273f6c1b12c04cc08b75
umair-moon-malik <tareekhkhan1426@gmail.com>
2024-11-21 19:52:07 +0530

    started working on study... layout is somewhat final

--- src/components/Study.jsx
+++ src/components/Study.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Study = () => {
   return (
-    <div>
-      Study Section
-    </div>
-  )
-}
+    <div className="study-main-container flex overflow-x-hidden max-w-[100vw]">
+      <div className="study-sidebar min-w-[20vw] max-w-[20vw] h-[86vh] bg-secondary rounded-[20px] ml-2 mr-2 mb-2">
+        <div className="study-sidebar-heading-wrapper m-2 overflow-x-hidden flex flex-col items-center mb-8">
+          <div className="w-full">
+          <h2 className="text-xl tracking-wider ml-1">Available Courses</h2>
+          </div>
+          <input type="text" className="w-full mt-2 p-[5px] rounded-md bg-[#1a1c2b] focus:outline-none border-b-[1px] border-gray-600 mb-[1.2rem]" placeholder="Search Cources by Course Code" />
+          <span className="min-w-[90%] border-b-[1px] border-gray-600 mb-[1.2rem]"></span>
+        </div>
+        
+      </div>
+      <div className="study-content-wrapper w-full h-[86vh] bg-secondary rounded-[20px] ml-2 mr-2 mb-2 overflow-x-hidden overflow-y-scroll hide-scrollbar">
 
-export default Study
+      </div>
+    </div>
+  );
+};
+
+export default Study;
