693e7392c86f400f5528faeeeb49e221fe0ae1a0
Yash Rawalkar <yashrawalkar04@gmail.com>
2025-11-19 22:44:38 +0530

    Test Commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,22 +22,21 @@ const App = () => {
   // Show loading while checking authentication
   if (loading) {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-[#080912]">
-        <div className="text-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto"></div>
-          <p className="mt-4 text-gray-300">Loading...</p>
+      <div className='min-h-screen flex items-center justify-center bg-[#080912]'>
+        <div className='text-center'>
+          <div className='animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto'></div>
+          <p className='mt-4 text-gray-300'>Loading...</p>
         </div>
       </div>
     )
   }
 
   return (
-    <div className="relative min-h-screen bg-[#080912]">
-      {/* Header with higher z-index to stay above background */}
+    <div className='relative min-h-screen bg-[#080912]'>
       <Header />
 
       {/* Main content area */}
-      <main className="relative z-10">
+      <main className='relative z-10'>
         <Routes>
           <Route path='/' element={<Hero />} />
           <Route path='/login' element={<Login />} />
